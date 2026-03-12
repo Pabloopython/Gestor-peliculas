686fe19940312b262eaf71bdc7bda7a3a1b52b85
Alejandro Muñoz Jiménez <amunjim1405@g.educaand.es>
2026-03-12 09:02:10 +0100

    c

--- main.js
+++ main.js
@@ -3,3 +3,15 @@ const vista = document.querySelector("mododevista")
 
 titulo.style.color = "red";
 
+const sizes = ["small", "medium", "large"];
+let current = 1; // empieza en medium
+
+button.addEventListener("click", () => {
+
+  articulos.classList.remove(sizes[current]);
+
+  current = (current + 1) % sizes.length;
+
+  articulos.classList.add(sizes[current]);
+
+});
