c82f9678b3e88406aee9bffec7f94550873f5634
Pablo Chacón Martínez <pchamar3107@g.educaand.es>
2026-03-12 09:30:48 +0100

    boton

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const titulo = document.querySelector("h1");
 const vista = document.querySelector("mododevista")
+const botonmodo = document.querySelector("#botonmodo");
 
 titulo.style.color = "red";
 
 const sizes = ["small", "medium", "large"];
 let current = 1; // empieza en medium
-
+/* 
 button.addEventListener("click", () => {
 
   articulos.classList.remove(sizes[current]);
@@ -14,4 +15,9 @@ button.addEventListener("click", () => {
 
   articulos.classList.add(sizes[current]);
 
+});
+ */
+botonmodo.addEventListener("click", function() {
+    console.log("modo");
+    document.body.classList.toggle("noche");
 });
