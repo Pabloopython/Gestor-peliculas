4837397348224c23d7266c77d11df78404eed69b
Alejandro Muñoz Jiménez <amunjim1405@g.educaand.es>
2026-04-23 08:38:47 +0200

    cambios

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 # Importamos la clase Flask desde el paquete instalado
 from flask import Flask, render_template
-
+import sqlite3  
 
 # Creamos una instancia de la aplicación. __name__ ayuda a Flask a localizar archivos
 app = Flask(__name__)
 
 # Definimos una ruta. El símbolo @ es un decorador que vincula la URL con la función de abajo
-@app.route("/") # Cuando el usuario entre en la dirección raíz (home)
-def inicio():
-    # Esta función devuelve el código HTML que el navegador mostrará al usuario
-    return render_template("index.html", titulo="GESTOR PELÍCULAS", mensaje="Bienvenido al repositorio de películas.")
+@app.route("/")
+def ver_coleccion():
+    # 1. Conectamos con el archivo de la base de datos
+    conexion = sqlite3.connect("peliculas_gestor.db")
+    
+    # 2. Configuramos la conexión para que devuelva diccionarios (más fácil para Jinja2)
+    conexion.row_factory = sqlite3.Row
+    cursor = conexion.cursor()
+    
+    # 3. Ejecutamos la consulta SQL
+    cursor.execute("SELECT * FROM Peliculas")
+    
+    # 4. Guardamos todos los resultados en una variable
+    datos = cursor.fetchall()
+    
+    # 5. Cerramos la conexión
+    conexion.close()
+    
+    # 6. Enviamos los datos reales a la plantilla
+    return render_template("index.html", items=datos)
 
 # Comprobamos si el script se está ejecutando directamente (y no importado como módulo)
 if __name__ == "__main__":
